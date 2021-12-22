7b2ee451423f4c50b80081172d3fe87fcdc991f6
Hamzah Nurwarid <hn.warid@gmail.com>
2021-12-23 01:58:25 +0700

    Initial commit

---
+++ 0 Python Fundamental/1_06_sequencetuple.py
@@ -0,0 +1,7 @@
+contoh_tuple = ('Januari', 'Februari', 'Maret', 'April')
+print(contoh_tuple[0])
+contoh_tuple = ('Januari', 'Februari', 'Maret', 'April')
+contoh_tuple[0] = 'Desember'
+# Traceback (most recent call last):
+#   File "<stdin>", line 4, in <module>
+# TypeError: 'tuple' object does not support item assignment
