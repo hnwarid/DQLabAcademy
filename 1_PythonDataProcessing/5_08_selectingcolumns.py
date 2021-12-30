7ddb3f43aed6aff8b6f1e85490af2c7a26f1970f
Hamzah Nurwarid <hn.warid@gmail.com>
2021-12-31 00:13:50 +0700

    Update README.md and Module 05 initial commit

---
+++ 1_PythonDataProcessing/5_08_selectingcolumns.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+raw_data = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/dataset_statistic.csv", sep=';')
+
+# Memilih kolom 'Pendapatan' saja
+print(raw_data['Pendapatan'])
+
+# Memilih kolom 'Jenis Kelamin' dan 'Pendapatan'
+print(raw_data[['Jenis Kelamin', 'Pendapatan']])
