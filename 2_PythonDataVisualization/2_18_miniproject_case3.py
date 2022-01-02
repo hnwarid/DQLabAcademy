be74c56b60bf1015e752447fb09bf8f9b3782891
Hamzah Nurwarid <hn.warid@gmail.com>
2022-01-02 12:17:09 +0700

    Update README.md & Module 2 plots & miniprojects

--- 2_PythonDataVisualization/2_18_miniproject_case3.py
+++ 2_PythonDataVisualization/2_18_miniproject_case3.py
@@ -4,16 +4,18 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
 dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
 dataset['gmv'] = dataset['item_price']*dataset['quantity']
-
-#mengambil informasi top 5 brands berdasarkan quantity
 top_brands = (dataset[dataset['order_month']=='2019-12'].groupby('brand')['quantity']
                 .sum()
                 .reset_index()
                 .sort_values(by='quantity',ascending=False)
                 .head(5))
-
-#membuat dataframe baru, filter hanya di bulan Desember 2019 dan hanya top 5 brands
 dataset_top5brand_dec = dataset[(dataset['order_month']=='2019-12') & (dataset['brand'].isin(top_brands['brand'].to_list()))]
 
-# print top brands
-print(top_brands)
+plt.clf()
+dataset_top5brand_dec.groupby('brand')['product_id'].nunique().sort_values(ascending=False).plot(kind='bar', color='green')
+plt.title('Number of Sold Product per Brand, December 2019',loc='center',pad=30, fontsize=15, color='blue')
+plt.xlabel('Brand', fontsize = 15)
+plt.ylabel('Number of Products',fontsize = 15)
+plt.ylim(ymin=0)
+plt.xticks(rotation=0)
+plt.show()
--- 2_PythonDataVisualization/2_20_miniproject_case5.py
+++ 2_PythonDataVisualization/2_20_miniproject_case5.py
@@ -4,16 +4,17 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
 dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
 dataset['gmv'] = dataset['item_price']*dataset['quantity']
-
-#mengambil informasi top 5 brands berdasarkan quantity
 top_brands = (dataset[dataset['order_month']=='2019-12'].groupby('brand')['quantity']
                 .sum()
                 .reset_index()
                 .sort_values(by='quantity',ascending=False)
                 .head(5))
-
-#membuat dataframe baru, filter hanya di bulan Desember 2019 dan hanya top 5 brands
 dataset_top5brand_dec = dataset[(dataset['order_month']=='2019-12') & (dataset['brand'].isin(top_brands['brand'].to_list()))]
 
-# print top brands
-print(top_brands)
+plt.figure(figsize=(10,5))
+plt.hist(dataset_top5brand_dec.groupby('product_id')['item_price'].median(), bins=10, stacked=True, range=(1,2000000), color='green')
+plt.title('Distribution of Price Median per Product\nTop 5 Brands in Dec 2019',fontsize=15, color='blue')
+plt.xlabel('Price Median', fontsize = 12)
+plt.ylabel('Number of Products',fontsize = 12)
+plt.xlim(xmin=0,xmax=2000000)
+plt.show()
